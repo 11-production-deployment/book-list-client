205392723472391b7cc116a2f2a53b8d8ac8fa09
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-04-14 17:19:35 -0700

    my eyes hurt

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -8,3 +8,28 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 if (ENV.isProduction){
   alert('hi');
 }
+
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+  function Task(taskObject) {
+    Object.keys(taskObject).forEach(key => this[key] = taskObject[key]);
+  }
+  Task.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#task-template').text());
+    return template(this);
+  }
+  Task.all = [];
+  Task.loadAll = rows => {
+    Task.all = rows.sort((a, b) => b.title - a.title).map(task => new Task(task));
+  }
+  Task.fetchAll = callback =>
+    $.get(`${ENV.apiUrl}/books`)
+      .then(Task.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+  module.Task = Task;
+})(app)
+
---
+++ scripts/views/book-view.js
@@ -0,0 +1,17 @@
+'use strict';
+var app = app || {};
+(function (module) {
+  const taskView = {};
+  taskView.initIndexPage = function () {
+    $('.container').hide();
+    $('.task-view').show();
+    console.log('init index page');
+    app.Task.all.map(task => $('#task-list').append(task.toHtml()));
+  };
+  module.taskView = taskView;
+})(app);
+
+$(function () {
+  console.log('in IIFE?');
+  app.Task.fetchAll(app.taskView.initIndexPage);
+});
